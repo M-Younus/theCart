6c5c2ed0ef82e92dfebb32815057bd70e41ec7b4
Muhammad Younas <MYounas@users.noreply.github.com>
2017-10-09 07:22:04 +0500

    theme integrating

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+from shop.models import Category,Product
+
+def index(request):
+    categories=list(Category.objects.all())
+    products=list(Product.objects.all())
+
+    return render(request,'shop/index.html',{
+        'categories':categories,
+        'products':products
+    });
+
