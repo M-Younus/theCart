4bb4faac0144271766060a3a8da1c0af4635be2a
Muhammad Younas <MYounas@users.noreply.github.com>
2018-01-20 17:56:46 +0500

    ...

--- theCart/urls.py
+++ theCart/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     url(r'^cart/', include('cart.urls', namespace='cart')),
     url(r'^(?P<CName>[-\w]+)/(?P<Pid>\d+)/$', views.product_detail, name='product_detail'),
     url(r'^(?P<cat_id>\d+)/$', views.cat_related_products,name="cat_related_products"),
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),##
 ]
 
 if settings.DEBUG:
